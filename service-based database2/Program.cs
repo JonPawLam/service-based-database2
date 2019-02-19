cfa42fcd87467d7504ad366f779add60a4662f22
Jon Lam <jonpawlam@gmail.com>
2019-02-19 13:58:03 +0100

    Add project files.

---
+++ service-based database2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace service_based_database2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
